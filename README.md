f708837d0216ab55d39f11830cde1c5fabe67f22
Filippo Conti <filippo@codekraft.it>
2018-01-15 12:40:42 +0100

    Package refactoring

---
+++ README.md
@@ -0,0 +1,39 @@
+# react-justified-layout
+A simple react component to deal with browser SpeechRecognition
+
+## Getting started
+Install the module via npm.
+```
+npm install react-justified-layout
+```
+Than import the script and the style to your project:
+```js
+import ReactSpeech from 'react-justified-layout';
+import 'react-justified-layout/build/css/index.css'
+```
+Now you can use the __react-justified-layout__ component everywhere in your application.
+
+## Development
+Clone the project repository, than install the dependencies and run:
+```
+npm run start
+```
+this will fire up the grunt development environment with a http server and actions on file changes, than when you have done your editing you can run:
+```
+npm run build
+```
+to package the project to be ready for production.
+
+---
+
+## License
+The __react-justified-layout__ is released under the MIT License by [codekraft-studio](https://codekraft-studio.github.io/).
+
+## Contributing
+
+1. Fork it ( https://github.com/codekraft-studio/react-justified-layout/fork )
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+3. Write and run the tests (`npm run test`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
---
+++ src/lib/components/JustifiedLayout.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import justifiedLayout from 'justified-layout';
+import './JustifiedLayout.css'
+
+class JustifiedLayout extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      containerStyle: {},
+      items: []
+    };
+
+    // Bind context to component functions
+    this.getBoxesGeometry = this.getBoxesGeometry.bind(this);
+    this.getBoxes = this.getBoxes.bind(this);
+  }
+
+  addPixels(string) {
+    return `${string}px`;
+  }
+
+  componentDidMount() {
+    // Once the component is ready init the view since it depends on element width
+    this.getBoxesGeometry();
+
+    // Add the window resize event listener to re-render
+    window.addEventListener('resize', this.getBoxesGeometry);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.getBoxesGeometry);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if( this.props === nextProps ) {
+      return;
+    }
+
+    // Ensure props are updated
+    this.props = nextProps;
+
+    // Get new geometry
+    this.getBoxesGeometry();
+  }
+
+  getBoxesGeometry() {
+    // update options container width with element inner width
+    this.props.options.containerWidth = this.refs.container.offsetWidth || this.refs.container.clientWidth;
+
+    console.log('Searching geom for:', this.props.items);
+    // get boxes geometry
+    this.geometry = justifiedLayout(this.props.items, this.props.options);
+
+    console.log('Found geometry:', this.geometry);
+
+    // update the state variables
+    this.setState({
+      items: this.getBoxes(),
+      containerStyle: {
+        position: 'relative',
+        height: this.addPixels(this.geometry.containerHeight)
+      }
+    });
+  }
+
+  getBoxes() {
+    return this.geometry.boxes.map((b, i) => {
+      let obj = typeof this.props.items[i] === 'object' ? this.props.items[i] : {}
+        return Object.assign(obj, {
+          style: {
+            height: `${b.height}px`,
+            width: `${b.width}px`,
+            top: `${b.top}px`,
+            left: `${b.left}px`,
+            position: 'absolute'
+          }
+        });
+    });
+  }
+
+  render() {
+
+    // Default rendering function
+    const renderBoxes = () => {
+
+			// if is a function execute it passing state items
+			if( typeof this.props.children === 'function' ) {
+				return this.props.children(this.state.items);
+			}
+
+			// if no template children map items with default template
+			if( this.props.children === null ) {
+	      return this.state.items.map(
+	        (item, index) => {
+	          return (
+	            <div key={index} style={item.style} className={item.className}>
+	              <img src={item.src} alt={item.title}></img>
+	            </div>
+	          );
+	        }
+	      );
+			}
+
+			// Use child template for create items
+			return React.Children.map(this.props.children, (child) => {
+	      // TODO: check if a self running function is seen as a valid element to avoid using extra components
+	      if( !child || child === null ) { return child; }
+	      return React.cloneElement(child, { items: this.state.items });
+	    });
+
+    };
+
+    return (
+      <div ref="container" className="justified-layout-container" style={this.state.containerStyle}>
+        { renderBoxes() }
+      </div>
+    );
+
+  }
+
+}
+
+JustifiedLayout.propTypes = {
+	children: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.node,
+		PropTypes.func,
+	]),
+  items: PropTypes.array.isRequired,
+  options: PropTypes.object,
+};
+
+JustifiedLayout.defaultProps = {
+  options: {},
+	children: null,
+  items: []
+};
+
+export default JustifiedLayout;
